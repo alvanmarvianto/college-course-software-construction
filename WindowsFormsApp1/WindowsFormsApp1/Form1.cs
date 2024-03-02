f09a3ae032082094604a77e9d2d7bb93f8599ace
Alvan <alvanmarvianto@gmail.com>
2024-03-02 18:04:33 +0700

    tambahan fitur GUI

---
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Kirim : Form
+    {
+        public Kirim()
+        {
+            InitializeComponent();
+        }
+
+        private void Input_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // Memunculkan output pada label2 jika button 1 diklik
+            label2.Text = "Selamat datang, " + textBox1.Text;
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
