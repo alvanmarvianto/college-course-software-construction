654cc2167c3c3774b3f10d88be1f78f77925cb61
Alvan <alvanmarvianto@gmail.com>
2024-03-10 17:30:19 +0700

    menambahkan implementasi dengan table-driven

--- tpmodul4_1302223074/Program.cs
+++ tpmodul4_1302223074/Program.cs
@@ -1,1 +1,31 @@
-﻿
+﻿public class Program
+{
+    public enum Kelurahan { 
+        Batununggal, Kujangsari, Mengger, Wates,
+        Cijaura, Jatisari, Margasari, Sekejati,
+        Kebonwaru, Maleer, Samoja
+    }
+    public class KodePos
+    {
+        public Kelurahan kelurahan;
+        public KodePos(Kelurahan inputKelurahan)
+        {
+            this.kelurahan = inputKelurahan;
+        }
+        int[] nomorKodePos = {
+            40266, 40287, 40267, 40256,
+            40287, 40286, 40286, 40286,
+            40272, 40274, 40273
+        };
+
+        public int getKodePos()
+        {
+            return nomorKodePos[(int)kelurahan];
+        }
+    }
+    public static void Main(string[] args)
+    {
+        KodePos k1 = new KodePos(Kelurahan.Batununggal);
+        Console.WriteLine("Kelurahan " + k1.kelurahan + " memiliki kode pos: " + k1.getKodePos());
+    }
+}
