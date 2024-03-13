51d2e119a30d2245e248743c37bf782d9f1e8a82
Alvan Marvianto <alvanm@student.telkomuniversity.ac.id>
2024-03-13 12:53:41 +0700

    menambahkan class kodeBuah

--- modul3_1302223074/Program.cs
+++ modul3_1302223074/Program.cs
@@ -1,1 +1,34 @@
-﻿
+﻿public class Program
+{
+    public enum NamaBuah
+    {
+        Apel, Aprikot, Alpukat, 
+        Pisang, Paprika, Blackberry,
+        Ceri, Kelapa, Jagung
+    }
+
+    public class KodeBuah
+    {
+        public NamaBuah namaBuah;
+        public KodeBuah(NamaBuah namaBuah)
+        {
+            this.namaBuah = namaBuah;
+        }
+        String[] nomorKodeBuah = {
+            "A00", "B00", "C00",
+            "D00", "E00", "F00",
+            "H00", "I00", "J00"
+        };
+        public String getKodeBuah()
+        {
+            return nomorKodeBuah[(int)namaBuah];
+        }
+    }
+
+    public static void Main(string[] args)
+    {
+        KodeBuah b1 = new KodeBuah(NamaBuah.Pisang);
+        Console.WriteLine("Buah " + b1.namaBuah + " memiliki kode: " + b1.getKodeBuah());
+        Console.WriteLine();
+    }
+}
--- modul3_1302223074/obj/Debug/net8.0/modul3_1302223074.AssemblyInfo.cs
+++ modul3_1302223074/obj/Debug/net8.0/modul3_1302223074.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("modul3_1302223074")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a38183cef0a3ee0745d54d7886c9634dff9b6a18")]
 [assembly: System.Reflection.AssemblyProductAttribute("modul3_1302223074")]
 [assembly: System.Reflection.AssemblyTitleAttribute("modul3_1302223074")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
