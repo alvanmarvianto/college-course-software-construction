42cadf3cb3d8ea8a31ca9c1c21d35e766aa29512
Alvan <alvanmarvianto@gmail.com>
2024-03-16 15:44:33 +0700

    menambahkan class DataGeneric

--- tpmodul5_1302223074/Program.cs
+++ tpmodul5_1302223074/Program.cs
@@ -1,1 +1,23 @@
-﻿
+﻿class Program
+{
+    public class DataGeneric<T>
+    {
+        T data;
+        public DataGeneric(T data)
+        {
+            this.data = data;
+        }
+
+        public void PrintData()
+        {
+            Console.WriteLine("Data yang tersimpan adalah: " + data);
+        }
+    }
+
+    static void Main(string[] args)
+    {
+        Console.WriteLine();
+        DataGeneric<string> dataObj = new DataGeneric<string>("1302223074");
+        dataObj.PrintData();
+    }
+}
