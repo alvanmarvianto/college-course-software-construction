110e59339642c666b1e9afd7293a13328101697f
Alvan <alvanmarvianto@gmail.com>
2024-03-23 12:33:06 +0700

    menambahkan class SayaTubeVideo

--- tpmodul6_1302223074/Program.cs
+++ tpmodul6_1302223074/Program.cs
@@ -2,8 +2,38 @@
 
 class Program
 {
+    public class SayaTubeVideo
+    {
+        private int id;
+        string title;
+        private int playCount;
+
+        public SayaTubeVideo(string title)
+        {
+            var rand = new Random();
+            this.id = rand.Next(9999);
+            this.title = title;
+            this.playCount = 0;
+        }
+        
+        public void IncreasePlayCount(int jmlh)
+        {
+            playCount += jmlh;
+        }
+
+        public void PrintVideoDetails()
+        {
+            Console.WriteLine("ID: " + id);
+            Console.WriteLine("Title: " + title);
+            Console.WriteLine("Play count: " + playCount);
+        }
+    }
+
+
     static void Main(string[] args)
     {
-
+        SayaTubeVideo vid1 = new SayaTubeVideo("Tutorial Design By Contract – Alvan Marvianto");
+        vid1.IncreasePlayCount(1000);
+        vid1.PrintVideoDetails();
     }
 }
