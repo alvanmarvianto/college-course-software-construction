10b6dcd8275eaa3c3291a7493e36279d2304bb22
Alvan Marvianto <alvanmarvianto@gmail.com>
2024-03-25 08:03:00 +0700

    penambahan design by contract pada semua kelas

--- modul6_1302223074/Program.cs
+++ modul6_1302223074/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Diagnostics;
+using System.Diagnostics.Contracts;
 
 class Program
 {
@@ -8,8 +10,13 @@ class Program
         private List<SayaTubeVideo> uploadedVideos;
         public String Username;
 
+
         public SayaTubeUser(String username)
         {
+            if (username == null)
+                throw new ArgumentNullException(nameof(username), "Username tidak boleh null.");
+            if (username.Length > 100)
+                throw new ArgumentException("Username tidak boleh lebih dari 100 karakter.", nameof(username));
             this.Username = username;
             var rand = new Random();
             this.id = rand.Next(9999);
@@ -28,6 +35,10 @@ class Program
 
         public void AddVideo(SayaTubeVideo video)
         {
+            if (video == null)
+                throw new ArgumentNullException(nameof(video), "Video tidak boleh null.");
+            if (video.getPlayCount() > int.MaxValue)
+                throw new ArgumentException("Jumlah playcount video tidak boleh lebih dari batas max integer.", nameof(video));
             uploadedVideos.Add(video);
         }
 
@@ -36,7 +47,13 @@ class Program
             Console.WriteLine("User: " + Username);
             for (int i = 0; i < uploadedVideos.Count; i++)
             {
-                Console.WriteLine("Video " + (i+1) + " judul: " + uploadedVideos[i].getTitle);
+                if (i > 7) 
+                {
+                    Console.WriteLine("Jumlah Video maksimal yang boleh diprint hanya 8");
+                    break;
+                }
+                Console.WriteLine("Video " + (i+1) + " judul: " + uploadedVideos[i].getTitle());
+                
             }
         }
     }
@@ -44,22 +61,39 @@ class Program
     public class SayaTubeVideo
     {
         private int id;
-        private String title;
+        private string title;
         private int playCount;
 
-        public SayaTubeVideo(String title)
+        public SayaTubeVideo(string title)
         {
+            if (title == null)
+                throw new ArgumentNullException(nameof(title), "Judul video tidak boleh null.");
+            if (title.Length > 200)
+                throw new ArgumentException("Judul video tidak boleh lebih dari 100 karakter.", nameof(title));
+
             var rand = new Random();
             this.id = rand.Next(9999);
             this.title = title;
             this.playCount = 0;
-            
         }
-        
 
         public void IncreasePlayCount(int jmlh)
         {
-            playCount += jmlh;
+            if (jmlh < 0 || jmlh > 25000000)
+                throw new ArgumentOutOfRangeException(nameof(jmlh),
+                    "Input penambahan play count harus antara 0 hingga 25,000,000.");
+
+            try
+            {
+                checked
+                {
+                    playCount += jmlh;
+                }
+            }
+            catch (OverflowException ex)
+            {
+                Console.WriteLine("Error: " + ex.Message);
+            }
         }
 
         public void PrintVideoDetails()
@@ -67,7 +101,7 @@ class Program
             Console.WriteLine("ID: " + id);
             Console.WriteLine("Title: " + title);
             Console.WriteLine("Play count: " + playCount);
-        }
+        }   
 
         public int getPlayCount()
         {
@@ -119,7 +153,69 @@ class Program
         Console.WriteLine(user1.Username + " memiliki total playcount: " + user1.GetTotalVideoPlayCount());
         Console.WriteLine();
 
-        user1.PrintAllVideoPlaycount();
-        Console.WriteLine();
+        try
+        {
+            user1.PrintAllVideoPlaycount();
+            Console.WriteLine();
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.ToString());
+        }
+
+        try
+        {
+            SayaTubeUser user2 = new SayaTubeUser(null);
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+
+        try
+        {
+            SayaTubeUser user3 = new SayaTubeUser("mmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmm");
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+
+        try
+        {
+            SayaTubeVideo vid11 = new SayaTubeVideo(null);
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("Error: " + ex.Message);
+        }
+
+        try
+        {
+            SayaTubeVideo vid12 = new SayaTubeVideo("mmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmm");
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("Error: " + ex.Message);
+        }
+
+        try
+        {
+            SayaTubeVideo vid13 = new SayaTubeVideo("Tutorial Design By Contract – Alvan Marvianto");
+            vid4.IncreasePlayCount(100000000);
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("Error: " + ex.Message);
+        }
+
+        SayaTubeVideo vid14 = new SayaTubeVideo("Tutorial Design By Contract – Alvan Marvianto");
+
+        for (int i = 0; i < 100; i++)
+        {
+            vid14.IncreasePlayCount(25000000);
+        }
+
+        vid14.PrintVideoDetails();
     }
 }
